e589fdd27da9c45c4e4521c4c673b59c0e654d01
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-01-26 22:12:02 -0600

    Agregar componente App con estructura inicial y contenido de bienvenida

---
+++ class1/src/App.jsx
@@ -0,0 +1,21 @@
+import MainLayout from './components/layout/MainLayout';
+
+function App() {
+  return (
+    <>
+      <MainLayout>
+        <h1>Hola Mundo</h1>
+        <h2>Bienvenidos a mi aplicación creada con React y Tailwind CSS</h2>
+        <article>
+          <p>
+            Esta es una aplicación de ejemplo para demostrar la estructura
+            básica de un proyecto React utilizando componentes reutilizables y
+            estilos con Tailwind CSS.
+          </p>
+        </article>
+      </MainLayout>
+    </>
+  );
+}
+
+export default App;
