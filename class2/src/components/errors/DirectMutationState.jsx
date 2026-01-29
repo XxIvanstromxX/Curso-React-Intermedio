8090bea65c0808062bbb7ea7baa3518b2a6c5a0f
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-01-28 22:56:13 -0600

    feat: agregar componentes AsyncState, DerivateState y DirectMutationState para gestión de estados en React que generar errores

---
+++ class2/src/components/errors/DirectMutationState.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+export function DirectMutationState() {
+  const [array, setArray] = useState([]);
+
+  function handleClick() {
+    array.push('Nuevo elemento');
+    setArray(array);
+    console.log('Array after push:', array);
+  }
+
+  return (
+    <div>
+      <h2>Direct Mutation of State Example</h2>
+      <button className="border-2" onClick={handleClick}>
+        Add Element
+      </button>
+      <ul>
+        {array.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export function CorrectMutationState() {
+  const [array, setArray] = useState([]);
+
+  function handleClick() {
+    setArray([...array, 'Nuevo elemento']);
+  }
+
+  return (
+    <div>
+      <h2>Correct Mutation of State Example</h2>
+      <button onClick={handleClick}>Add Element</button>
+      <ul>
+        {array.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
