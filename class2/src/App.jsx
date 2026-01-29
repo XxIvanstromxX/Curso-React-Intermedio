8dfda82ecd3bcaa8a339a70c84a960966c82aa11
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-01-28 22:55:39 -0600

    feat: agregar archivos App.jsx y main.jsx con configuración inicial de la aplicación

---
+++ class2/src/App.jsx
@@ -0,0 +1,14 @@
+import MainLayout from './components/layout/MainLayout';
+import ClientsPage from './pages/ClientsPage';
+
+function App() {
+  return (
+    <>
+      <MainLayout>
+        <ClientsPage />
+      </MainLayout>
+    </>
+  );
+}
+
+export default App;
