8c40ffb3d5744adb493fcf10ea37c321cf3a3c19
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-04 22:09:25 -0600

    feat: agregar manejo de estado para la carga y errores en clientsReducer

--- MiniCRM/src/reducers/clientsReducer.js
+++ MiniCRM/src/reducers/clientsReducer.js
@@ -1,9 +1,32 @@
 export const initialState = {
   clients: [],
+  loading: false,
+  error: null,
 };
 
 export function clientsReducer(state, action) {
   switch (action.type) {
+    case 'FETCH_CLIENTS_START':
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case 'FETCH_CLIENTS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        clients: action.payload,
+      };
+
+    case 'FETCH_CLIENTS_ERROR':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
     case 'ADD_CLIENT':
       return {
         ...state,
