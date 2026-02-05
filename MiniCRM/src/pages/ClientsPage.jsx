c7de809265ca0b4af949fd60d1d5e669ad667e40
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-04 22:09:16 -0600

    feat: integrar hook useClients en ClientsPage para gestionar la obtención de clientes

--- MiniCRM/src/pages/ClientsPage.jsx
+++ MiniCRM/src/pages/ClientsPage.jsx
@@ -1,12 +1,20 @@
 import ClientsList from '../components/clients/ClientList';
 import ClientActions from '../components/clients/ClientActions';
+import { useClients } from '../hooks/useClients';
 
 export default function ClientsPage() {
+  const { clients, loading, error } = useClients();
+
   return (
     <div>
       <h1>Clients Management</h1>
+
+      {loading && <p>Cargando Clientes...</p>}
+
+      {error && <p>Error {error}</p>}
+
       <ClientActions />
-      <ClientsList />
+      <ClientsList clients={clients} />
     </div>
   );
 }
