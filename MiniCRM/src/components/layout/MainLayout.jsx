454fd33dbbdf3c2b41f63b70c8ed64810774fb39
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-04 22:09:01 -0600

    fix: corregir el color de fondo del componente MainLayout

--- MiniCRM/src/components/layout/MainLayout.jsx
+++ MiniCRM/src/components/layout/MainLayout.jsx
@@ -5,7 +5,7 @@ export default function MainLayout({ children }) {
     <div className="min-h-screen flex flex-col">
       <Header />
 
-      <main className="flex-1 bg-amber-200">{children}</main>
+      <main className="flex-1 bg-amber-50">{children}</main>
 
       <footer className="text-center"> Hecho con amor a la programación</footer>
     </div>
