47dd81b970a6b07487e25f5bf9e7f114b3ba79ac
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-06 22:16:53 -0600

    feat: agregar funcionalidad de edición de clientes en ClientList

--- MiniCRM/src/components/clients/ClientList.jsx
+++ MiniCRM/src/components/clients/ClientList.jsx
@@ -1,4 +1,9 @@
+import ClientForm from './ClientForm';
+import { useState } from 'react';
+
 export default function ClientList({ clients }) {
+  const [editingClient, setEditingClient] = useState(false);
+
   return (
     <ul>
       {clients.map((client) => (
@@ -7,6 +12,22 @@ export default function ClientList({ clients }) {
           <button className="bg-red-500 rounded-md text-white ml-2.5 px-2 py-0.5 border-amber-50 border">
             Remove
           </button>
+          {/*Boton Editar Cliente*/}
+          <button
+            onClick={() =>
+              setEditingClient(editingClient === client.id ? false : client.id)
+            }
+          >
+            {editingClient === client.id ? 'Cancelar' : 'Edit'}
+          </button>
+          {editingClient === client.id && (
+            <ClientForm
+              existingClient={client}
+              onClose={() => {
+                setEditingClient(false);
+              }}
+            />
+          )}
         </li>
       ))}
     </ul>
