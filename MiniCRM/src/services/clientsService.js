ed874f8dc561f5e1a33b1fa30d7666b682f4aded
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-06 22:17:07 -0600

    fix: corregir errores tipográficos en la función getClients y mejorar la legibilidad del código

--- MiniCRM/src/services/clientsService.js
+++ MiniCRM/src/services/clientsService.js
@@ -2,13 +2,59 @@ const API_URL = 'https://69840783885008c00dafdee6.mockapi.io/api/clients';
 
 export async function getClients() {
   try {
-    const reponse = await fetch(API_URL);
-    const data = await reponse.json();
+    const response = await fetch(API_URL);
+    const data = await response.json();
     return data;
   } catch (error) {
     throw new Error('Error fetching clients: ' + error.message);
   }
 }
+
+export const addClient = async (client) => {
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(client),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('Error al añadir un client' + error);
+  }
+};
+
+export const updateClient = async (id, client) => {
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(client),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('Error al actualizar un client' + error);
+  }
+};
+
+/*
+Esto es un objeto de js
+const objeto = {
+  usuario: "usuario 1",
+}
+*/
+
+/*
+Esto es un JSON
+{
+  "usuario": "usuario 1",
+}
+*/
 
 // Alternativa con .then() basada en promesas es menos recomendable
 // function getClients(){
