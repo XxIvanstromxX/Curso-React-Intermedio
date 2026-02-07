cb65c950ca5dac3398b0c7148c752caa857ebad6
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-06 22:17:00 -0600

    feat: agregar funcionalidad para gestionar la apertura del formulario de cliente

--- MiniCRM/src/pages/ClientsPage.jsx
+++ MiniCRM/src/pages/ClientsPage.jsx
@@ -1,19 +1,25 @@
 import ClientsList from '../components/clients/ClientList';
-import ClientActions from '../components/clients/ClientActions';
+import ClientForm from '../components/clients/ClientForm';
 import { useClients } from '../hooks/useClients';
+import { useState } from 'react';
 
 export default function ClientsPage() {
   const { clients, loading, error } = useClients();
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <div>
       <h1>Clients Management</h1>
 
+      <button onClick={() => setModalOpen(!modalOpen)}>
+        {modalOpen ? 'Cancelar' : 'Agregar Cliente'}
+      </button>
+
       {loading && <p>Cargando Clientes...</p>}
 
       {error && <p>Error {error}</p>}
 
-      <ClientActions />
+      {modalOpen && <ClientForm existingClient={null} />}
       <ClientsList clients={clients} />
     </div>
   );
