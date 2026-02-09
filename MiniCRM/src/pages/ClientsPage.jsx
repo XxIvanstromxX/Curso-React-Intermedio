c6565cc05c3407a8a346b86b90a9152c6ed97ea7
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-09 15:58:03 -0600

    feat: mejorar el diseño de la página de clientes con estilos y estructura responsiva

--- MiniCRM/src/pages/ClientsPage.jsx
+++ MiniCRM/src/pages/ClientsPage.jsx
@@ -8,18 +8,25 @@ export default function ClientsPage() {
   const [modalOpen, setModalOpen] = useState(false);
 
   return (
-    <div>
-      <h1>Clients Management</h1>
+    <div className="max-w-7xl mx-auto p-4 flex flex-col gap-4">
+      <h1 className="text-3xl font-bold text-gray-700 mb-3">
+        Clients Management
+      </h1>
 
-      <button onClick={() => setModalOpen(!modalOpen)}>
-        {modalOpen ? 'Cancelar' : 'Agregar Cliente'}
+      <button
+        onClick={() => setModalOpen(!modalOpen)}
+        className="bg-blue-500 hover:bg-blue-600 text-white px-5 py-2 rounded-md text-lg mb-4 self-end-safe"
+      >
+        Agregar Cliente
       </button>
 
       {loading && <p>Cargando Clientes...</p>}
 
       {error && <p>Error {error}</p>}
 
-      {modalOpen && <ClientForm existingClient={null} />}
+      {modalOpen && (
+        <ClientForm existingClient={null} onClose={() => setModalOpen(false)} />
+      )}
       <ClientsList clients={clients} />
     </div>
   );
