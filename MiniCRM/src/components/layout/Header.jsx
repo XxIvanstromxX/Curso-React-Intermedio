4373d9a9fbe8ec830cd2d56caa496e76afb93fe7
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-09 15:57:56 -0600

    feat: rediseñar el encabezado con búsqueda y botones de usuario estilizados

--- MiniCRM/src/components/layout/Header.jsx
+++ MiniCRM/src/components/layout/Header.jsx
@@ -1,14 +1,54 @@
+import search from '../../assets/search.svg';
+
 export default function Header() {
   return (
-    <nav className="flex items-center bg-gray-800 justify-between p-4 text-white">
-      <h1>My Aplication</h1>
-      <ul className="flex gap-4">
-        <li className="underline">
-          <a href="">home</a>
-        </li>
-        <li className="underline">About</li>
-        <li className="underline">Contact</li>
-      </ul>
+    <nav className="relative flex items-center justify-between bg-[#f9f9f9] p-4 shadow-md max-w-7xl  mx-auto w-7xl mt-3 mb-4 rounded-3xl">
+      <div>
+        <h1 className="text-xl font-bold text-gray-700">MiniCRM</h1>
+      </div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-md">
+        <img
+          src={search}
+          alt="Search"
+          className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-900 w-5 h-5 pointer-events-none"
+        />
+
+        <input
+          type="text"
+          placeholder="Search by Id, Email or Phone"
+          className="pl-11 py-1 text-gray-600 placeholder-gray-600 max-w-md w-full rounded-xl focus:outline-none focus:ring-1 focus:ring-blue-200 focus:border-transparent bg-[#ececec]"
+        />
+      </div>
+      <div>
+        <div className="flex flex-row gap-5 items-center justify-center">
+          <div>
+            <button className="px-3 py-1 cursor-pointer">
+              <img
+                src="https://img.icons8.com/?size=100&id=9GvqBKJLhLDa&format=png&color=000000"
+                alt="notification"
+                className="w-6 h-6"
+              />
+            </button>
+          </div>
+          <stoke className="text-gray-600 text-2xl">|</stoke>
+          <div className="flex flex-row items-center justify-center">
+            <button className="flex flex-row items-center justify-center cursor-pointer">
+              <div className="flex flex-col text-gray-600 items-end">
+                <h3 className="font-light text-sm">User</h3>
+                <p className="font-light text-sm">Admin</p>
+              </div>
+            </button>
+
+            <button className="flex items-center justify-center cursor-pointer">
+              <img
+                src="https://img.icons8.com/?size=100&id=20749&format=png&color=000000"
+                alt="user"
+                className="w-10 h-10 rounded-full ml-3"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
