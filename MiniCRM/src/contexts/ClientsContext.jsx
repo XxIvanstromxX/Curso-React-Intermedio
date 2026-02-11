ed2255f31350516964d1aff854ac63aaa2bf7aeb
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-02-10 22:04:15 -0600

    feat: agregar uso de useMemo para optimizar el contexto de clientes

--- MiniCRM/src/contexts/ClientsContext.jsx
+++ MiniCRM/src/contexts/ClientsContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useMemo } from 'react';
 import { clientsReducer, initialState } from '../reducers/clientsReducer';
 
 export const ClientsContext = createContext();
@@ -6,8 +6,10 @@ export const ClientsContext = createContext();
 export function ClientsProvider({ children }) {
   const [state, dispatch] = useReducer(clientsReducer, initialState);
 
+  const memoizedValue = useMemo(() => ({ state, dispatch }), [state, dispatch]);
+
   return (
-    <ClientsContext.Provider value={{ state, dispatch }}>
+    <ClientsContext.Provider value={memoizedValue}>
       {children}
     </ClientsContext.Provider>
   );
